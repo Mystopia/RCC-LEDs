f2dd5af61dd582788c09e9c2f182fb49f1386e35
Mystopia <mystopia@Mickeys-MacBook-Air.local>
2015-08-22 22:52:32 -0400

    Switch to sensible defaults for the wall

--- examples/python/chase.py
+++ examples/python/chase.py
@@ -4,12 +4,13 @@
 
 import opc, time
 
-numLEDs = 128 * 10
+numLEDs = 64 * 8 * 4
 client = opc.Client('localhost:7890')
 
 while True:
 	for i in range(numLEDs):
+		print i
 		pixels = [ (0,0,0) ] * numLEDs
 		pixels[i] = (255, 255, 255)
 		client.put_pixels(pixels)
-		time.sleep(0.0025)
+		time.sleep(0.01)
